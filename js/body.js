6286ddce6ebd89c7f85210e6369bdd5ebef8cbc9
xzyweb <98642251+xzyweb@users.noreply.github.com>
2022-01-29 18:29:52 +0800

    Add files via upload

---
+++ js/body.js
@@ -0,0 +1,76 @@
+getbody();
+function getbody(){
+	if (!getvalue('page')) {
+	    $("#body").html('<h3>页面载入出错！</h3>')
+		return;
+	} else {
+	    var pagev = getvalue('page')
+	}
+	
+	var body = "";
+	$.get({
+	    url: "https://api.github.com/repos/xzyweb/xzyweb.github.io/issues/" + pagev,
+	    dataType: "json",
+	    success: function(data) {
+	        if (!data.hasOwnProperty('message')) {
+	            $.post({
+	                url: "https://api.github.com/markdown",
+	                data: JSON.stringify({
+	                    'text': data.body
+	                }),
+	                dataType: "html",
+	                success: function(res) {
+	                    var d = new Date(data.created_at);
+	                    var ctime = d.getFullYear() + '-' + d.getMonth() + 1 + '-' + d.getDate() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
+	                    var d = new Date(data.updated_at);
+	                    var ltime = d.getFullYear() + '-' + d.getMonth() + 1 + '-' + d.getDate() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
+	                    var body=res.substr(res.split("\n")[0].length);
+						$("#body").html('<h3>' + data.title + '</h3><p>创建时间：' + ctime + '<br>最后修改时间：' + ltime + '</p><hr>' + body)
+					},
+	                error: function(data) {
+	                    if (data.status == 403) {
+	                        $("#body").html('<h3>' + data.title + '</h3><p>创建时间：' + ctime + '<br>最后修改时间：' + ltime + '</p><hr>' + '<p>操作过于频繁，请稍后再试</p>');
+	                    } else if (data.status == 0) {
+	                        $("#body").html('<h3>' + data.title + '</h3><p>创建时间：' + ctime + '<br>最后修改时间：' + ltime + '</p><hr>' + '<p>无法连接至服务器，请检查网络设置</p>');
+	                    } else if (data.status == 404) {
+	                        $("#body").html('<h3>' + data.title + '</h3><p>创建时间：' + ctime + '<br>最后修改时间：' + ltime + '</p><hr>' + '<p>系统异常，请稍后再试</p>');
+	                    } else if (data.status == 200) {
+	                        $("#body").html('<h3>' + data.title + '</h3><p>创建时间：' + ctime + '<br>最后修改时间：' + ltime + '</p><hr>' + '<p>系统异常，请重新刷新后再试</p>');
+	                    } else {
+	                        alert(data.msg);
+	                        $("#body").html('<h3>' + data.title + '</h3><p>创建时间：' + ctime + '<br>最后修改时间：' + ltime + '</p><hr>' + '<p>' + data.msg + '</p>');
+	                    }
+	                }
+	            })
+	        } else {
+				$("#body").html('<h3>系统异常，请重新刷新后再试</h3>');
+	        }
+	    },
+	    error: function(data) {
+	        if (data.status == 403) {
+	            $("#body").html('<h3>加载出错了!</h3><hr><p>操作过于频繁，请稍后再试</p>');
+	        } else if (data.status == 0) {
+	            $("#body").html('<h3>加载出错了!</h3><hr><p>无法连接至服务器，请检查网络设置</p>');
+	        } else if (data.status == 404) {
+	            $("#body").html('<h3>加载出错了!</h3><hr><p>文章不存在</p>');
+	        } else if (data.status == 410) {
+	            $("#body").html('<h3>加载出错了!</h3><hr><p>文章已被删除</p>');
+	        } else if (data.status == 200) {
+	            $("#body").html('<h3>加载出错了!</h3><hr><p>系统异常，请重新刷新后再试</p>');
+	        } else {
+	            $("#body").html('<h3>加载出错了!</h3><hr><p>' + data.msg + '</p>');
+	        }
+	    }
+	})
+}
+function getvalue(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == variable) {
+            return pair[1]
+        }
+    }
+    return (false)
+}
---
+++ js/main.js
@@ -0,0 +1,80 @@
+$.get({
+    url: "https://api.github.com/repos/xzyweb/xzyweb.github.io/issues?state=open&per_page=10&page=1&labels=置顶,资源",
+    dataType: "json",
+    success: function(data) {
+        if (!data.hasOwnProperty('message')) {
+			var topup ="";
+			var body="";
+            for (i = 0; i in data; i++) {
+                var label = "";
+                var img = "";
+				var top = false;
+                for (n = 0; n in data[i].labels; n++) {
+					if(data[i].labels[n].name=="置顶"){
+						var top=true;
+					}else{
+						var label = label + '<span class="badge rounded-pill bg-primary">' + data[i].labels[n].name + '</span> ';
+					}
+                }
+				if(top==true){
+					var topup = topup + '<a href="./body.html?page=' + data[i].number + '" class="list-group-item list-group-item-action"><h4><span class="badge rounded-pill bg-primary">置顶</span>' + data[i].title.substr(4) + '</h4><p>'+ data[i].body.split("\n")[0]+'</p>' + label + '</a>';
+				}else{
+					var body = body +'<a href="./body.html?page=' + data[i].number + '" class="list-group-item list-group-item-action"><h4>' + data[i].title + '</h4><p>'+ data[i].body.split("\n")[0]+'</p>' + label + '</li>';
+				}
+			}
+			$('#list').append(topup);
+			$('#list').append(body);
+        } else {
+            alert('系统异常，请重新刷新后再试')
+        }
+    },
+    error: function(data) {
+        if (data.status == 403) {
+            alert('操作过于频繁，请稍后再试')
+        } else if (data.status == 0) {
+            alert('无法连接至服务器，请检查网络设置')
+        } else if (data.status == 404) {
+            alert('系统异常，请稍后再试')
+        } else if (data.status == 200) {
+            alert('系统异常，请重新刷新后再试')
+        } else {
+            alert(data.msg)
+        }
+    }
+});
+$.get({
+    url: "https://api.github.com/repos/xzyweb/xzyweb.github.io/issues?state=open&per_page=5&page=1",
+    dataType: "json",
+    success: function(data) {
+        if (!data.hasOwnProperty('message')) {
+			var top ="";
+			var body="";
+            for (i = 0; i in data; i++) {
+                var label = "";
+                var img = "";
+                for (n = 0; n in data[i].labels; n++) {
+                    var label = label + '<span class="badge rounded-pill bg-primary">' + data[i].labels[n].name + '</span> '
+                }
+				if(data[i].title.substr(0,4)!=="[置顶]"){
+					var body = body +'<a href="./body.html?page=' + data[i].number + '" class="list-group-item list-group-item-action"><h4>' + data[i].title + '</h4><p>'+ data[i].body.split("\n")[0]+'</p>' + label + '</li>';
+				}
+			}
+			$('#list_right').append(body);
+        } else {
+            alert('系统异常，请重新刷新后再试')
+        }
+    },
+    error: function(data) {
+        if (data.status == 403) {
+            alert('操作过于频繁，请稍后再试')
+        } else if (data.status == 0) {
+            alert('无法连接至服务器，请检查网络设置')
+        } else if (data.status == 404) {
+            alert('系统异常，请稍后再试')
+        } else if (data.status == 200) {
+            alert('系统异常，请重新刷新后再试')
+        } else {
+            alert(data.msg)
+        }
+    }
+});
