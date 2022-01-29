50ec529c7b7f1876d6dc1c4fcdfd9a98106546d0
xzyweb <98642251+xzyweb@users.noreply.github.com>
2022-01-29 21:10:30 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,44 @@
 $.get({
-    url: "https://api.github.com/repos/xzyweb/xzyweb.github.io/issues?state=open&per_page=10&page=1&labels=置顶;广告;手机资源;电脑资源;工具推荐",
+    url: "https://api.github.com/repos/xzyweb/xzyweb.github.io/issues?state=open&labels=置顶",
     dataType: "json",
     success: function(data) {
         if (!data.hasOwnProperty('message')) {
-			var topup ="";
+			var body="";
+            for (i = 0; i in data; i++) {
+                var label = "";
+                var img = "";
+				var top = false;
+                for (n = 0; n in data[i].labels; n++) {
+					if(data[i].labels[n].name!=="置顶"){
+						var label = label + '<span class="badge rounded-pill bg-primary">' + data[i].labels[n].name + '</span> ';
+					}
+                }
+				var body = body + '<a href="./body.html?page=' + data[i].number + '" class="list-group-item list-group-item-action"><h4><span class="badge rounded-pill bg-primary">置顶</span>' + data[i].title + '</h4><p>'+ data[i].body.split("\n")[0]+'</p>' + label + '</a>';
+			}
+			$('#list').append(body);
+        } else {
+            alert('系统异常，请重新刷新后再试')
+        }
+    },
+    error: function(data) {
+        if (data.status == 403) {
+            alert('操作过于频繁，请稍后再试')
+        } else if (data.status == 0) {
+            alert('无法连接至服务器，请检查网络设置')
+        } else if (data.status == 404) {
+            alert('系统异常，请稍后再试')
+        } else if (data.status == 200) {
+            alert('系统异常，请重新刷新后再试')
+        } else {
+            alert(data.msg)
+        }
+    }
+});
+$.get({
+    url: "https://api.github.com/repos/xzyweb/xzyweb.github.io/issues?state=open&per_page=10&page=1",
+    dataType: "json",
+    success: function(data) {
+        if (!data.hasOwnProperty('message')) {
 			var body="";
             for (i = 0; i in data; i++) {
                 var label = "";
@@ -11,18 +46,15 @@ $.get({
 				var top = false;
                 for (n = 0; n in data[i].labels; n++) {
 					if(data[i].labels[n].name=="置顶"){
-						var top=true;
+						var top = true;
 					}else{
 						var label = label + '<span class="badge rounded-pill bg-primary">' + data[i].labels[n].name + '</span> ';
 					}
                 }
-				if(top==true){
-					var topup = topup + '<a href="./body.html?page=' + data[i].number + '" class="list-group-item list-group-item-action"><h4><span class="badge rounded-pill bg-primary">置顶</span>' + data[i].title + '</h4><p>'+ data[i].body.split("\n")[0]+'</p>' + label + '</a>';
-				}else{
-					var body = body +'<a href="./body.html?page=' + data[i].number + '" class="list-group-item list-group-item-action"><h4>' + data[i].title + '</h4><p>'+ data[i].body.split("\n")[0]+'</p>' + label + '</li>';
+				if(top==false){
+					var body = body + '<a href="./body.html?page=' + data[i].number + '" class="list-group-item list-group-item-action"><h4>' + data[i].title + '</h4><p>'+ data[i].body.split("\n")[0]+'</p>' + label + '</a>';
 				}
 			}
-			$('#list').append(topup);
 			$('#list').append(body);
         } else {
             alert('系统异常，请重新刷新后再试')
